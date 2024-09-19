6c4f52cbbb8a5340a59179dc7ec9916adfa8d729
Isaias L. Salinas <91746863+isalorsaw@users.noreply.github.com>
2024-09-18 20:08:44 -0600

    Introduccion BlueJ 18/09

---
+++ 1erParcial/PrimerEjercicio.java
@@ -0,0 +1,9 @@
+//Primer Ejercicio en Java
+//Pedir Dos enteros y mostrar la suma de ellos.
+public class PrimerEjercicio
+{
+    public static void main(String args[])
+    {
+        //Trabajar
+    }
+}
