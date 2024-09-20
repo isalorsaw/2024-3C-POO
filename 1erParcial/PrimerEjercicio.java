30fbc6c266770ca695333f1259d9ba6caf243918
Isaias L. Salinas <91746863+isalorsaw@users.noreply.github.com>
2024-09-19 20:03:19 -0600

    Primer Ejercicio en Java 19/8

--- 1erParcial/PrimerEjercicio.java
+++ 1erParcial/PrimerEjercicio.java
@@ -1,9 +1,32 @@
 //Primer Ejercicio en Java
 //Pedir Dos enteros y mostrar la suma de ellos.
+//Librerias
+import javax.swing.JOptionPane;//Paneles
 public class PrimerEjercicio
 {
     public static void main(String args[])
     {
-        //Trabajar
+        //Inicializacion de Variables
+        int numero1=0;
+        int numero2=0;
+        int suma=0;
+        
+        //Entradas
+        String entrada=JOptionPane.showInputDialog("Ingrese el Primer Numero");
+        numero1=Integer.parseInt(entrada);
+        
+        entrada=JOptionPane.showInputDialog("Ingrese el segundo Numero");
+        numero2=Integer.parseInt(entrada);
+        
+        //Proceso de Suma
+        suma=numero1+numero2;
+        
+        //String salida= numero1  +   "+"   + numero2   +   "="   + suma;
+        String salida="La Suma de "+numero1+" y "+numero2+" es: "+suma;
+
+        //Salida
+        //String salida="La Suma de los dos Numeros es: "+suma;
+        //String salida=numero1+"+"+numero2+"="+suma;
+        JOptionPane.showMessageDialog(null,salida,"Ventana",1);
     }
 }
